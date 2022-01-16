fe421c7ca198070ae53336f6b5997d5d895c3060
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-16 23:02:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -502,7 +502,7 @@ function makeAPIKey(/*length = 5*/) {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var charactersLength = characters.length;
   for(let i=0;i<11;i++){
-  result =
+  result +=
     characters.charAt(Math.floor(Math.random() * charactersLength)) ;
   }
   return result;
