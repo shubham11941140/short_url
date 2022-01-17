9c2a7fccc89a21149d1b4c213517612887f8be12
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-01-17 14:49:38 +0530

    Add files via upload

--- performance.py
+++ performance.py
@@ -1,5 +1,8 @@
 import requests
 import sys
+import time
+
+start_time = time.time()
 
 headers = {
     'Content-type': 'application/json',
@@ -14,34 +17,36 @@ website = sys.argv[3]
 
 if func == 1: # To Read
     count = int(sys.argv[4])
-    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:3000/api/readshorten/:{0}'.format(key)
+    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:7000/api/readshorten/{0}'.format(key)
     data = '{{"shorturl":"{0}"}}'.format(website)
     for i in range(1, count + 1):
-        print("CURL API REQUEST:", i)
         response = requests.post(route, headers=headers, data=data)
+        print("CURL REQUEST:", i, "GIVES RESPONSE:", response.text)
 
 if func == 2: # To Delete
-    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:3000/api/deleteurl/:{0}'.format(key)
+    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:7000/api/deleteurl/{0}'.format(key)
     data = '{{"shorturl":"{0}"}}'.format(website)
     response = requests.post(route, headers=headers, data=data)
+    print("RESPONSE IS:", response.text)
 
 if func == 3: # To Shorten
-    key = sys.argv[2]
-    website = sys.argv[3]
     count = int(sys.argv[4])
-    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:3000/api/createshorten/:{0}'.format(key)
+    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:7000/api/createshorten/{0}'.format(key)
     data = '{{"urldata":"{0}"}}'.format(website)
     for i in range(1, count + 1):
-        print("CURL API REQUEST:", i)
         response = requests.post(route, headers=headers, data=data)
+        print("CURL REQUEST:", i, "GIVES RESPONSE:", response.text)
 
 if func == 4: # To Create Custom
     custom = sys.argv[4]
     livetime = int(sys.argv[5])
-    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:3000/api/createcustomshorten/:{0}'.format(key)
-    data = '{{"customurldataa":"{0}", "longurldata":"{1}", "ttl":"{2}"}}'.format(custom, website, livetime)
+    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:7000/api/createcustomshorten/{0}'.format(key)
+    data = '{{"customurldata":"{0}", "longurldata":"{1}", "ttl":"{2}"}}'.format(custom, website, livetime)
     response = requests.post(route, headers=headers, data=data)
+    print("RESPONSE IS:", response.text)
 
+end_time = time.time()
+print("EXECUTION TIME", end_time - start_time)
 
 '''
 # To read
