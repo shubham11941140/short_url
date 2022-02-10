ee904cf24835d563f1ef347f77fe37c8dd399d45
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2022-02-10 13:15:17 +0530

    Add files via upload

--- performance.py
+++ performance.py
@@ -14,24 +14,24 @@ headers = {
 func = int(sys.argv[1])
 key = sys.argv[2]
 website = sys.argv[3]
-
+path = "http://fault-tolerance-eb-short-url-dev.us-east-1.elasticbeanstalk.com/api"
 if func == 1: # To Read
     count = int(sys.argv[4])
-    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:7000/api/readshorten/{0}'.format(key)
+    route = path + '/readshorten/{0}'.format(key)
     data = '{{"shorturl":"{0}"}}'.format(website)
     for i in range(1, count + 1):
         response = requests.post(route, headers=headers, data=data)
         print("CURL REQUEST:", i, "GIVES RESPONSE:", response.text)
 
 if func == 2: # To Delete
-    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:7000/api/deleteurl/{0}'.format(key)
+    route = path + '/deleteurl/{0}'.format(key)
     data = '{{"shorturl":"{0}"}}'.format(website)
     response = requests.post(route, headers=headers, data=data)
     print("RESPONSE IS:", response.text)
 
 if func == 3: # To Shorten
     count = int(sys.argv[4])
-    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:7000/api/createshorten/{0}'.format(key)
+    route = path + '/createshorten/{0}'.format(key)
     data = '{{"urldata":"{0}"}}'.format(website)
     for i in range(1, count + 1):
         response = requests.post(route, headers=headers, data=data)
@@ -40,7 +40,7 @@ if func == 3: # To Shorten
 if func == 4: # To Create Custom
     custom = sys.argv[4]
     livetime = int(sys.argv[5])
-    route = 'http://ec2-54-173-198-224.compute-1.amazonaws.com:7000/api/createcustomshorten/{0}'.format(key)
+    route = path + '/createcustomshorten/{0}'.format(key)
     data = '{{"customurldata":"{0}", "longurldata":"{1}", "ttl":"{2}"}}'.format(custom, website, livetime)
     response = requests.post(route, headers=headers, data=data)
     print("RESPONSE IS:", response.text)
@@ -78,6 +78,6 @@ data = '{{"url":"{0}"}}'.format(website)
 
 for i in range(1, count + 1):
     print("CURL API REQUEST:", i)
-    response = requests.post('http://ec2-54-173-198-224.compute-1.amazonaws.com:3000/api/createcustomshorten/:apiKey', headers=headers, data=data)
+    response = requests.post('http://fault-tolerance-eb-short-url-dev.us-east-1.elasticbeanstalk.com/:3000/api/createcustomshorten/:apiKey', headers=headers, data=data)
 
 '''
